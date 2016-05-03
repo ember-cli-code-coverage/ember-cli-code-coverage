8c8a7347e2782bd926d09a3d7029a59c96bb591c
Katie Gengler <katie@kmg.io>
2016-05-03 16:13:58 -0400

    Bump some timeouts by an order of magnitude

--- test/integration/coverage-test.js
+++ test/integration/coverage-test.js
@@ -18,7 +18,7 @@ describe('`ember test`', function() {
   });
 
   it('runs coverage when env var is set', function() {
-    this.timeout(10000);
+    this.timeout(100000);
     expect(dir('coverage')).to.not.exist;
     return runCommand('ember', ['test'], {env: {COVERAGE: true}}).then(function() {
       expect(file('coverage/lcov-report/index.html')).to.not.be.empty;
@@ -29,7 +29,7 @@ describe('`ember test`', function() {
   });
 
   it('does not run coverage when env var is NOT set', function() {
-    this.timeout(10000);
+    this.timeout(100000);
     expect(dir('coverage')).to.not.exist;
     return runCommand('ember', ['test']).then(function() {
       expect(dir('coverage')).to.not.exist;
