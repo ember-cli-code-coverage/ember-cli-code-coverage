0c21496826b4a0217a5ff6d98464d458389c830c
Paul Watts <paulcwatts@gmail.com>
2017-06-07 19:11:21 -0700

    Upgrade ember-exam
    This ensures it works around
    https://github.com/trentmwillis/ember-exam/issues/81

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,6 +3,8 @@ import {
   setResolver
 } from 'ember-qunit';
 import { start } from 'ember-cli-qunit';
+import loadEmberExam from 'ember-exam/test-support/load';
 
+loadEmberExam();
 setResolver(resolver);
 start();
