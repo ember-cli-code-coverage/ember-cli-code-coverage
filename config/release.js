f4734b7194714e821b4d35088299d0428c209510
Katie Gengler <katie@kmg.io>
2018-02-01 01:29:45 -0500

    Setup travis ci to release on pushed tag, add lerna-changelog
    - Documented in RELEASE.md

--- config/release.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const generateChangelog = require('ember-cli-changelog/lib/tasks/release-with-changelog');
-
-module.exports = {
-  publish: true,
-  beforeCommit: generateChangelog
-};
