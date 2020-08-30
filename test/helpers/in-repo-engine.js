82be8a15ac126b0e16ff41a7cb753c40aff40005
Tobias Bieniek <tobias@bieniek.cloud>
2020-08-30 10:03:55 +0200

    tests: Use async/await

--- test/helpers/in-repo-engine.js
+++ test/helpers/in-repo-engine.js
@@ -3,7 +3,7 @@
 const InRepoAddon = require('./in-repo-addon');
 
 class InRepoEngine extends InRepoAddon {
-  static generate(app, name, options) {
+  static async generate(app, name, options) {
     let args = ['generate', 'in-repo-engine', name];
 
     if (typeof options.lazy !== 'undefined') {
@@ -14,39 +14,39 @@ class InRepoEngine extends InRepoAddon {
       args.push('--type', options.type);
     }
 
-    return app.runEmberCommand.apply(app, args).then(() => {
-      let engine = new InRepoEngine(app, name);
-      // Set up a simple default CSS file
-      engine.writeFixture({
-        addon: {
-          styles: {
-            'app.css': `/* ${name}.css */`,
-          },
+    await app.runEmberCommand.apply(app, args);
+
+    let engine = new InRepoEngine(app, name);
+    // Set up a simple default CSS file
+    engine.writeFixture({
+      addon: {
+        styles: {
+          'app.css': `/* ${name}.css */`,
         },
-      });
-      return engine;
+      },
     });
+    return engine;
   }
 
-  generateNestedEngine(name) {
+  async generateNestedEngine(name) {
     // Generate another in-repo-engine at the app level...
     let args = Array.prototype.slice.call(arguments);
     args.unshift(this.app);
-    return InRepoEngine.generate.apply(null, args).then(engine => {
-      // Remove the in-repo-engine from the app...
-      this.app.editPackageJSON(pkg => {
-        pkg['ember-addon'].paths = pkg['ember-addon'].paths.filter(path => path !== `lib/${name}`);
-      });
+    let engine = await InRepoEngine.generate.apply(null, args);
 
-      // Add the in-repo-engine to this engine.
-      this.editPackageJSON(pkg => {
-        pkg['ember-addon'] = pkg['ember-addon'] || {};
-        pkg['ember-addon'].paths = pkg['ember-addon'].paths || [];
-        pkg['ember-addon'].paths.push(`../${name}`);
-      });
-
-      return engine;
+    // Remove the in-repo-engine from the app...
+    this.app.editPackageJSON(pkg => {
+      pkg['ember-addon'].paths = pkg['ember-addon'].paths.filter(path => path !== `lib/${name}`);
     });
+
+    // Add the in-repo-engine to this engine.
+    this.editPackageJSON(pkg => {
+      pkg['ember-addon'] = pkg['ember-addon'] || {};
+      pkg['ember-addon'].paths = pkg['ember-addon'].paths || [];
+      pkg['ember-addon'].paths.push(`../${name}`);
+    });
+
+    return engine;
   }
 }
 
