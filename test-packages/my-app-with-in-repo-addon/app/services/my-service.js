0533678d8a884080a7128d51e3221cd7afcd3e93
Garrick Cheung <gcheung@fitbit.com>
2020-10-07 14:59:53 -0700

    Add service and tests to replicate incorrect paths that are included for Istanbul.

---
+++ test-packages/my-app-with-in-repo-addon/app/services/my-service.js
@@ -0,0 +1,7 @@
+import MyService from 'my-in-repo-addon/services/my-service';
+
+export default MyService.extend({
+    testFunc() {
+        return true;
+    }
+});
---
+++ test-packages/my-app-with-in-repo-addon/lib/my-in-repo-addon/app/services/my-service.js
@@ -0,0 +1,1 @@
+export { default } from 'my-in-repo-addon/services/my-service';
