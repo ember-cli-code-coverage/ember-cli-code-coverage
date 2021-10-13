5644a747c9ec6255f65056bb1f6da1283c5ebc04
Travis Hoover <thoov7@gmail.com>
2021-10-13 11:45:38 -0700

    fixing lint issues

--- packages/ember-cli-code-coverage/.eslintrc.js
+++ packages/ember-cli-code-coverage/.eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
         './blueprints/*/index.js',
         './config/**/*.js',
         './tests/dummy/config/**/*.js',
+        './lib/**/*.js',
       ],
       parserOptions: {
         sourceType: 'script',
